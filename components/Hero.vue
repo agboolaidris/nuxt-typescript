2244d6bc5bd72c479e7cd909a0b2400aad28293e
idris agboola <agboolaisholaidreez@gmail.com>
2021-08-24 05:37:59 +0100

    update

---
+++ components/Hero.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container mx-auto d-flex">
+    <div>
+      <h1>Find your extingisher</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam odit
+        similique, alias doloremque reiciendis autem ipsam facere, totam impedit
+        amet consectetur tenetur distinctio fugit, placeat magni fuga vel neque
+        eveniet?
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="min-h-screen bg-gray-100">
+    <Hero />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
